8bec6fca42684941cf2bde5c4ed84c3a7576a69e
arraisi <itsarraisi@gmail.com>
2019-02-05 22:23:24 +0700

    Initial Commit

---
+++ hustle-mode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by Abdul R Arraisi on 05/02/19.
+//  Copyright © 2019 Abdul R Arraisi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
